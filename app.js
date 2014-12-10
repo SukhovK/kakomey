071428035a5b67199d6f651bf092fe9e39fae801
ksukhov <k.sukhov@dengionline.com>
2014-12-10 20:57:40 +0300

    tt

--- app.js
+++ app.js
@@ -29,75 +29,44 @@ passport.deserializeUser(function(id, done) {
 	var user = users[id];
 	done(err, user);
 });
-passport.use(new LocalStrategy(
-	function(username, password, done) {
-		var client = mysql.createClient({
-			user : 'username',
-			password: 'password'
-		});
-		client.query('USE nodetest2');
-		client.query('SELECT userid, password, salt FROM user WHERE username = ?',
-		[username], function(err, result, fields) {
-			// database error
-			if (err) {
-				return done(err);
-		// username not found
-			} else if (result.length == 0) {
-				return done(null, false, {message: 'Unknown user ' + username});
-// check password
-			} else {
-				var newhash = crypto.createHash('sha512')
-					.update(result[0].salt + password)
-					.digest('hex');
-// if passwords match
-				if (result[0].password === newhash) {
-					var user = {id : result[0].userid,
-								username : username,
-								password : newhash };
-					return done(null, user);
-					// else if passwords don't match
-					} else {
-						return done(null, false, {message: 'Invalid password'});
-					}
-				}
-			client.end();
-		});
-	})
+
+passport.use(new localStrategy(
+  function(username, password, done) { 
+    users.forEach(function(item){
+      if(item.username == username){
+	 if(item.password= password){
+	    return done(null, user);
+	 } else {
+	  return done(null, false, {message: 'Invalid password'});
+	}
+      }
+    });
+    
+    return done(null, false, {message: 'Unknown user ' + username});
+ 
+  })
 );
 
-client.query('USE nodetest2');
-client.query('SELECT userid, password, salt FROM user WHERE username = ?',
+
 app.configure(function(){
     app.set('view engine', 'jade');
     app.set('view options', { layout: true });
     app.set('views', __dirname + '/views');	
     app.use(express.favicon());
 	//app.use(express.logger());
-
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(app.router);
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
 	//app.use(express.cookieDecoder());
-
-	app.use(session({
+    app.use(session({
 		secret: 'keyboard cat',
 		resave: false,
 		saveUninitialized: true
 	}))
 	/////////////////////////////
-	app.use(passport.initialize()); 
-	app.use(passport.session());
-	passport.serializeUser(function(user, done) {
-		done(null, user._id);
-	}); 
-	passport.deserializeUser(function(id, done) {
-		User.findById(id, function(err, user) {
-			done(err, user); 
-		);
-	})
+    app.use(passport.initialize()); 
+    app.use(passport.session());
 
-	
-//////////////////////////////////////////
 
     app.use(lessMiddleware(__dirname + '/views', {
        dest: __dirname + '/public'
@@ -110,6 +79,7 @@ app.configure(function(){
 app.use(function(req, res, next){
   throw new Error(req.url + ' not found');
 });
+
 app.use(function(err, req, res, next) {
     console.log(err);
     res.send(err.message);
@@ -132,7 +102,7 @@ var prefixes = ['bands','musicians','records'];
 prefixes.forEach(function(prefix) {
     map.route(app, prefix);
 });
-http.createServer(app).listen(8080);
+http.createServer(app).listen(8070);
 console.log("Express server listening on port 8080");
 
 function snifle(arr) {
