4220c6c91d0b8cab44df1a807be0d103808b3252
KSukhov <sukhov-kirill@yandex.ru>
2016-04-12 20:38:56 +0300

    revival

--- utils/albums.js
+++ utils/albums.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
+
 var RecordModel    = require('../models/records').RecordModel;
 RecordModel.find({},function (err, records) {
    console.log(records)
