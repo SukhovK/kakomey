5875a3328cac64dc94e6c3ab217822b28b048bdb
KSukhov <ksukhov@yandex.ru>
2016-11-24 21:20:51 +0300

    add google analitic

---
+++ public/javascripts/ms.js
@@ -0,0 +1,20 @@
+
+.* var $container = $('.corp');
+// Инициализация
+$container.masonry({
+  columnWidth: 232,
+  gutter: 10,
+  itemSelector: '.note'
+});
+
+*/
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-12984834-2', 'auto');
+  ga('send', 'pageview');
+
+
